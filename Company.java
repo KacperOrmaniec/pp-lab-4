34f206efc36aa048b6a2c6f307dd29b22d58d936
Kacper Ormaniec <kacperormaniec@gmail.com>
2024-05-04 14:10:29 +0200

    zad3

--- Company.java
+++ Company.java
@@ -6,16 +6,26 @@ public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
 
-        employees[0] = new Manager("Kacper Ormaniec", 50000, 0);
-        employees[1] = new Worker("Julia Ormaniec", 60000, "HR");
-        employees[2] = new Employee("Jan Kowalski", 55000);
-        employees[3] = new Employee("Mikołaj Strzelczyk", 52000);
-        employees[4] = new Worker("Stanisław Polański", 58000, "Marketing");
+        employees[0] = new Manager("Kacper Ormaniec", 5000, 0);
+        employees[1] = new Worker("Julia Ormaniec", 6000, "HR");
+        employees[2] = new Employee("Jan Kowalski", 5500);
+        employees[3] = new Employee("Mikołaj Strzelczyk", 5200);
+        employees[4] = new Worker("Stanisław Polański", 5800, "Marketing");
 
         System.out.println("Dane pracownika o indeksie 3:");
         System.out.println(employees[3]);
 
         employees[3].setSalary(54000);
+
+        int nonManagerCount = 0;
+        for (Employee employee : employees) {
+            if (!(employee instanceof Manager)) {
+                nonManagerCount++;
+            }
+        }
+
+        ((Manager) employees[0]).setNumberOfSubordinates(nonManagerCount);
+        employees[0].setSalary(7500);
 
         System.out.println("\nDane wszystkich pracowników:");
         for (Employee employee : employees) {
